57e3016c469bc806b0f0a186124bbf4ed48a7225
Denis Kachan <den.kachan.98@mail.ru>
2023-10-31 21:40:58 +0300

    1. A few changes

--- src/StructureOfTheBank/HRDepartment.java
+++ src/StructureOfTheBank/HRDepartment.java
@@ -2,17 +2,17 @@ package StructureOfTheBank;
 
 public class HRDepartment extends BaseDepartment {
 
-    private int amountOfEmployees;
+    private int totalAmountOfEmployees;
 
     public HRDepartment(String address, int numberOfEmployees, int neededAmountOfEmployees) {
         super(address, numberOfEmployees, neededAmountOfEmployees);
     }
 
     public int getAmountOfEmployees() {
-        return amountOfEmployees;
+        return totalAmountOfEmployees;
     }
 
     public void setAmountOfEmployees(int amountOfEmployees) {
-        this.amountOfEmployees = amountOfEmployees;
+        this.totalAmountOfEmployees = amountOfEmployees;
     }
 }
