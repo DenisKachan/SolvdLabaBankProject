f641007e69434242cd6188533214a86fb67e9030
Denis Kachan <den.kachan.98@mail.ru>
2023-11-05 02:50:18 +0300

    small mistakes fixed

--- src/ClientsOfTheBank/BaseClient.java
+++ src/ClientsOfTheBank/BaseClient.java
@@ -55,7 +55,6 @@ public abstract class BaseClient {
         return commissionFee;
     }
 
-
     public void setCommissionFee(double commissionFee) {
         this.commissionFee = commissionFee;
     }
