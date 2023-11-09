9ca614761dca4640fb0d6b5d61289a5bb935ce28
DenisKachan <den.kachan.98@mail.ru>
2023-11-09 16:36:42 +0300

    little fixes

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,6 @@
-import ClientsOfTheBank.ClientsIndividuals;
-import ConsoleScanner.CreationObjectsFromConsole;
-import Exceptions.*;
-
 public class Main {
-    public static void main(String[] args) throws NameException, TotalAccountBalanceException,
-            AmountOfMonthlyIncomeException, AgeException, CreditDelaysException, AccountIdNumberException {
-        try (CreationObjectsFromConsole.scanner) {
-            CreationObjectsFromConsole creationObjectsFromConsole = new CreationObjectsFromConsole();
-            ClientsIndividuals clientsIndividuals = creationObjectsFromConsole.createClientIndividualFromConsole();
-            clientsIndividuals.withdrawCash();
-            clientsIndividuals.toTopUpBalance();
-            clientsIndividuals.transferMoney();
-        }
+
+    public static void main(String[] args) {
+
     }
 }
